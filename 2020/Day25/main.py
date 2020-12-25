dbe66340474b499b2af6b9b0f74252bb60098c46
Enrique Sandoval <ee.sandoval.jr@gmail.com>
2020-12-24 21:57:44 -0800

    Adding Day25

---
+++ 2020/Day25/main.py
@@ -0,0 +1,29 @@
+from math import log
+
+
+def part_one(card_public, door_public):
+    current = 1
+    card_loop = 0
+    while current != card_public:
+        current *= 7
+        current %= 20201227
+        card_loop += 1
+    print(card_loop)
+    card_loop = int(card_loop)
+    result = 1
+    for _ in range(card_loop):
+        result *= door_public
+        result %= 20201227
+    return result
+
+
+def part_two():
+    return ":)"
+
+
+with open("input.txt") as f:
+    card_public, door_public = f.read().splitlines()
+    card_public = int(card_public)
+    door_public = int(door_public)
+    print(part_one(card_public, door_public))
+    print(part_two())
