e03f8af0c11e88fe184d5a48996ec5d19c1fd9cc
eesandoval <ee.sandoval.jr@gmail.com>
2022-12-03 10:49:38 -0800

    Updated README, fixed spacing and missing make

--- 2022/Day01/main.cpp
+++ 2022/Day01/main.cpp
@@ -9,57 +9,57 @@ using namespace std;
 
 vector<string> readInputVector(string fileName)
 {
-    ifstream fin(fileName);
-    vector<string> output;
-    string temp;
-    while (getline(fin, temp))
-    {
-        output.push_back(temp);
-    }
-    return output;
+	ifstream fin(fileName);
+	vector<string> output;
+	string temp;
+	while (getline(fin, temp))
+	{
+		output.push_back(temp);
+	}
+	return output;
 }
 
 vector<int> sumAllCalories(vector<string> allCalories)
 {
-    int tempTotalCalories = 0;
-    vector<int> summedCalories;
-    for (string calorieStr : allCalories)
-    {
-        if (calorieStr.empty())
-        {
-            summedCalories.push_back(tempTotalCalories);
-            tempTotalCalories = 0;
-        }
-        else
-        {
-            tempTotalCalories += stoi(calorieStr);
-        }
-    }
-    summedCalories.push_back(tempTotalCalories);
-    sort(summedCalories.begin(), summedCalories.end(), greater<int>());
-    return summedCalories;
+	int tempTotalCalories = 0;
+	vector<int> summedCalories;
+	for (string calorieStr : allCalories)
+	{
+		if (calorieStr.empty())
+		{
+			summedCalories.push_back(tempTotalCalories);
+			tempTotalCalories = 0;
+		}
+		else
+		{
+			tempTotalCalories += stoi(calorieStr);
+		}
+	}
+	summedCalories.push_back(tempTotalCalories);
+	sort(summedCalories.begin(), summedCalories.end(), greater<int>());
+	return summedCalories;
 }
 
 int partOne(vector<int> summedCalories)
 {
-    return summedCalories[0];
+	return summedCalories[0];
 }
 
 int partTwo(vector<int> summedCalories)
 {
-    int result = 0;
-    for (int i = 0; i < 3; i++)
-    {
-        result += summedCalories[i];
-    }
-    return result;
+	int result = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		result += summedCalories[i];
+	}
+	return result;
 }
 
 int main(int argc, char* argv[])
 {
-    vector<string> allCalories = readInputVector(argv[1]);
-    vector<int> summedCalories = sumAllCalories(allCalories);
-    cout << partOne(summedCalories) << endl;
-    cout << partTwo(summedCalories) << endl;
-    return 0;
+	vector<string> allCalories = readInputVector(argv[1]);
+	vector<int> summedCalories = sumAllCalories(allCalories);
+	cout << partOne(summedCalories) << endl;
+	cout << partTwo(summedCalories) << endl;
+	return 0;
 }  
--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# AdventOfCode - Advent of Code Solutions in Python
+# AdventOfCode - Advent of Code Solutions in Python and C++
 ## Requirements
 - [Python 3.6 or higher](https://www.python.org/)
+- GCC for the C++ solutions
 
 ## Running These Solutions
+### Python 2020 and 2021
 Navigate to the year/day's folder and execute the below command
 ```
 python3 main.py
+```
+### C++ 2022
+Navigate to the year/day's folder and execute the below command
+```
+make
+./output input
 ```
