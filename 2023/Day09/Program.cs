1ef7cc66c885ef2a818e1d939bc39f234a590a2c
Enrique Sandoval <ee.sandoval.jr@gmail.com>
2023-12-08 23:05:36 -0800

    Adding Day09

---
+++ 2023/Day09/Program.cs
@@ -0,0 +1,38 @@
+﻿int getNewNumber(List<int> history, bool next = true)
+{
+    var deltas = new List<int>();
+    for (var i = 0; i < history.Count - 1; i++)
+    {
+        deltas.Add(history[i+1] - history[i]);
+    }
+    if (deltas.Distinct().Count() == 1)
+    {
+        return history[next ? history.Count - 1 : 0] + deltas[0] * (next ? 1 : -1);
+    }
+    return history[next ? history.Count - 1 : 0] + getNewNumber(deltas, next) * (next ? 1 : -1);
+}
+
+int analyzeHistory(string[] allLines, bool partTwo = false)
+{
+    var result = 0;
+    foreach (var line in allLines)
+    {
+        var history = line.Split(' ').Select(int.Parse).ToList();
+        result += getNewNumber(history, !partTwo);
+    }
+    return result;
+}
+
+int partOne(string[] allLines)
+{
+    return analyzeHistory(allLines);
+}
+
+int partTwo(string[] allLines)
+{
+    return analyzeHistory(allLines, true);
+}
+
+var allLines = File.ReadAllLines("input");
+Console.WriteLine(partOne(allLines));
+Console.WriteLine(partTwo(allLines));
