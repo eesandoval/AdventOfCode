35ee5fe47aca5e8f3c738f5e30a17767ff9ddc72
Enrique Sandoval <ee.sandoval.jr@gmail.com>
2023-12-10 01:07:23 -0800

    Day10 clean up

--- 2023/Day10/Program.cs
+++ 2023/Day10/Program.cs
@@ -7,61 +7,57 @@
     {
         for (var c = 0; c < allLines[r].Length; c++)
         {
-            if (allLines[r][c] == 'S') // assume it's F
+            switch (allLines[r][c])
             {
-                start = (r, c);
-            }
-            if (allLines[r][c] == '|') // up and down
-            {
-                pipes[(r, c)] = new List<(int, int)>()
-                {
-                    (r+1, c),
-                    (r-1, c)
-                };
-            }
-            else if (allLines[r][c] == '-') // left and right
-            {
-                pipes[(r, c)] = new List<(int, int)>()
-                {
-                    (r, c+1),
-                    (r, c-1)
-                };
-            }
-            else if (allLines[r][c] == 'L') // up and right
-            {
-                pipes[(r, c)] = new List<(int, int)>()
-                {
-                    (r-1, c),
-                    (r, c+1)
-                };
-            }
-            else if (allLines[r][c] == 'J') // up and left
-            {
-                pipes[(r, c)] = new List<(int, int)>()
-                {
-                    (r-1, c),
-                    (r, c-1)
-                };
-            }
-            else if (allLines[r][c] == '7') // down and left
-            {
-                pipes[(r, c)] = new List<(int, int)>()
-                {
-                    (r+1, c),
-                    (r, c-1)
-                };
-            }
-            else if (allLines[r][c] == 'F' || allLines[r][c] == 'S') // down and right
-            {
-                pipes[(r, c)] = new List<(int, int)>()
-                {
-                    (r+1, c),
-                    (r, c+1)
-                };
-            }
-            else // .
-            {
-                pipes[(r, c)] = new List<(int, int)>();
+                case 'S': // example inputs and my own use F for S
+                          // A generalized approach would be better but it was quick to eyeball
+                case 'F': // down and right
+                    if (allLines[r][c] == 'S')
+                        start = (r, c);
+                    pipes[(r, c)] = new List<(int, int)>()
+                    {
+                        (r+1, c),
+                        (r, c+1)
+                    };
+                    break;
+                case '|': // up and down
+                    pipes[(r, c)] = new List<(int, int)>()
+                    {
+                        (r+1, c),
+                        (r-1, c)
+                    };
+                    break;
+                case '-': // left and right
+                    pipes[(r, c)] = new List<(int, int)>()
+                    {
+                        (r, c+1),
+                        (r, c-1)
+                    };
+                    break;
+                case 'L': // up and right
+                    pipes[(r, c)] = new List<(int, int)>()
+                    {
+                        (r-1, c),
+                        (r, c+1)
+                    };
+                    break;
+                case 'J': // up and left
+                    pipes[(r, c)] = new List<(int, int)>()
+                    {
+                        (r-1, c),
+                        (r, c-1)
+                    };
+                    break;
+                case '7': // down and left
+                    pipes[(r, c)] = new List<(int, int)>()
+                    {
+                        (r+1, c),
+                        (r, c-1)
+                    };
+                    break;
+                default: // .
+                    pipes[(r, c)] = new List<(int, int)>();
+                    break;
             }
             distances[(r, c)] = -1;
         }
@@ -70,19 +66,14 @@
     // DFS store distances in an a dictionary for each step, overwriting the -1
     var queue = new Queue<(int, int)>();
     queue.Enqueue(start);
-    var explored = new HashSet<(int, int)>
-    {
-        start
-    };
     distances[start] = 0;
     while (queue.Count > 0)
     {
         var next = queue.Dequeue();
         foreach (var edge in pipes[next])
         {
-            if (!explored.Contains(edge))
+            if (distances[edge] == -1)
             {
-                explored.Add(edge);
                 queue.Enqueue(edge);
                 distances[edge] = distances[next] + 1;
             }
@@ -95,7 +86,6 @@
         if (distances[d] == -1)
         {
             distances.Remove(d);
-            pipes.Remove(d);
         }
     }
     return distances;
@@ -122,7 +112,6 @@ int partTwo(string[] allLines)
     }
     for (var r = 0; r < allLines.Length; r++)
     {
-        var row = new List<char>();
         for (var c = 0; c < allLines[r].Length; c++)
         {
             if (distances.ContainsKey((r, c)))
@@ -131,42 +120,36 @@ int partTwo(string[] allLines)
                 {
                     case '|':
                         upscaledGrid[r*3][c*3 + 1] = '|';
-                        upscaledGrid[r*3 + 1][c*3 + 1] = '|'; // center
                         upscaledGrid[r*3 + 2][c*3 + 1] = '|';
                         break;
                     case '-':
                         upscaledGrid[r*3 + 1][c*3] = '-';
-                        upscaledGrid[r*3 + 1][c*3 + 1] = '-'; // center
                         upscaledGrid[r*3 + 1][c*3 + 2] = '-';
                         break;
                     case 'L':
                         upscaledGrid[r*3][c*3 + 1] = '|';
-                        upscaledGrid[r*3 + 1][c*3 + 1] = 'L'; // center
                         upscaledGrid[r*3 + 1][c*3 + 2] = '-';
                         break;
                     case 'J':
                         upscaledGrid[r*3][c*3 + 1] = '|';
-                        upscaledGrid[r*3 + 1][c*3 + 1] = 'J'; // center
                         upscaledGrid[r*3 + 1][c*3] = '-';
                         break;
                     case '7':
                         upscaledGrid[r*3 + 1][c*3] = '-';
-                        upscaledGrid[r*3 + 1][c*3 + 1] = '7'; // center
                         upscaledGrid[r*3 + 2][c*3 + 1] = '|';
                         break;
                     case 'F':
                         upscaledGrid[r*3 + 1][c*3 + 2] = '-';
-                        upscaledGrid[r*3 + 1][c*3 + 1] = 'F'; // center
                         upscaledGrid[r*3 + 2][c*3 + 1] = '|';
                         break;
                     case 'S': // I didn't account that S is a + and the last example had a hole when expanded...
                         upscaledGrid[r*3][c*3 + 1] = '|';
                         upscaledGrid[r*3 + 2][c*3 + 1] = '|';
-                        upscaledGrid[r*3 + 1][c*3 + 1] = 'S'; // center
                         upscaledGrid[r*3 + 1][c*3] = '-';
                         upscaledGrid[r*3 + 1][c*3 + 2] = '-';
                         break;
                 }
+                upscaledGrid[r*3 + 1][c*3 + 1] = allLines[r][c]; // center
             }
             else
             {
@@ -180,10 +163,6 @@ int partTwo(string[] allLines)
     // Use DFS to fill starting from the top left corner (0, 0)
     var queue = new Queue<(int, int)>();
     queue.Enqueue((0, 0));
-    var explored = new HashSet<(int, int)>()
-    {
-        (0, 0)
-    };
     while (queue.Count > 0)
     {
         var n = queue.Dequeue();
@@ -191,19 +170,19 @@ int partTwo(string[] allLines)
         if (upscaledGrid[n.Item1][n.Item2] == ' ' || upscaledGrid[n.Item1][n.Item2] == 'I')
         {
             upscaledGrid[n.Item1][n.Item2] = 'O';
-            if (n.Item1 - 1 > 0 && !explored.Contains((n.Item1 - 1, n.Item2)))
+            if (n.Item1 - 1 > 0)
             {
                 queue.Enqueue((n.Item1 - 1, n.Item2));
             }
-            if (n.Item1 + 1 < upscaledGrid.Count && !explored.Contains((n.Item1 + 1, n.Item2)))
+            if (n.Item1 + 1 < upscaledGrid.Count)
             {
                 queue.Enqueue((n.Item1 + 1, n.Item2));
             }
-            if (n.Item2 - 1 > 0 && !explored.Contains((n.Item1, n.Item2 - 1)))
+            if (n.Item2 - 1 > 0)
             {
                 queue.Enqueue((n.Item1, n.Item2 - 1));
             }
-            if (n.Item2 + 1 < upscaledGrid[0].Count && !explored.Contains((n.Item1, n.Item2 + 1)))
+            if (n.Item2 + 1 < upscaledGrid[0].Count)
             {
                 queue.Enqueue((n.Item1, n.Item2 + 1));
             }
